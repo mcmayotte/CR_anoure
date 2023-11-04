d20b35523bd83d3220a231048171643ef9f8a701
mcmayotte <marie-claude.mayotte@usherbrooke.ca>
2023-11-04 19:05:48 -0400

    Préparations données pour analyse licat et lisyl

--- Scripts/analyse_stat.R
+++ Scripts/analyse_stat.R
@@ -1,15 +1,10 @@
-library(unmarked)
-
 #------------------------------
 # Ouvrir données
 #------------------------------
 #Données réponse
-#licat_21_15h <- read.csv('donnees/licat_21_15h.csv', header = T)
-licat_21_21h <- read.csv('donnees/licat_21_21h.csv', header = T)
 licat_21_1h <- read.csv('donnees/licat_21_1h.csv', header = T)
-#licat_22_15h <- read.csv('donnees/licat_21_15h.csv', header = T)
-licat_22_21h <- read.csv('donnees/licat_21_21h.csv', header = T)
 licat_22_1h <- read.csv('donnees/licat_21_1h.csv', header = T)
+lisyl_22_21h <- read.csv('donnees/lisyl_21_21h.csv', header = T)
 
 #Données occupation
 MH <- read.csv("donnees/MH_nett.csv", header = T)
@@ -17,20 +12,49 @@ route <- read.csv("donnees/routes_nett.csv", header = T)
 territoire <- read.csv("donnees/utilisation_territoire_nett.csv", header = T)
 
 #Données détection
-qualite_2021_15h <- read.csv('donnees/qualite_2021_15h.csv', header = T)
-qualite_2021_15h <- read.csv('donnees/qualite_2021_21h.csv', header = T)
-qualite_2021_15h <- read.csv('donnees/qualite_2021_1h.csv', header = T)
-qualite_2021_15h <- read.csv('donnees/qualite_2022_15h.csv', header = T)
-qualite_2021_15h <- read.csv('donnees/qualite_2022_21h.csv', header = T)
-qualite_2021_15h <- read.csv('donnees/qualite_2022_1h.csv', header = T)
+#qualite_2021_15h <- read.csv('donnees/qualite_2021_15h.csv', header = T)
+#qualite_2021_21h <- read.csv('donnees/qualite_2021_21h.csv', header = T)
+qualite_2021_1h <- read.csv('donnees/qualite_2021_1h.csv', header = T)
+#qualite_2022_15h <- read.csv('donnees/qualite_2022_15h.csv', header = T)
+qualite_2022_21h <- read.csv('donnees/qualite_2022_21h.csv', header = T)
+qualite_2022_1h <- read.csv('donnees/qualite_2022_1h.csv', header = T)
 
-disturb_2021_15h <- read.csv('donnees/disturb_2021_15h.csv', header = T)
-disturb_2021_15h <- read.csv('donnees/disturb_2021_21h.csv', header = T)
-disturb_2021_15h <- read.csv('donnees/disturb_2021_1h.csv', header = T)
-disturb_2021_15h <- read.csv('donnees/disturb_2022_15h.csv', header = T)
-disturb_2021_15h <- read.csv('donnees/disturb_2022_21h.csv', header = T)
-disturb_2021_15h <- read.csv('donnees/disturb_2022_1h.csv', header = T)
+#disturb_2021_15h <- read.csv('donnees/disturb_2021_15h.csv', header = T)
+#disturb_2021_21h <- read.csv('donnees/disturb_2021_21h.csv', header = T)
+disturb_2021_1h <- read.csv('donnees/disturb_2021_1h.csv', header = T)
+#disturb_2022_15h <- read.csv('donnees/disturb_2022_15h.csv', header = T)
+disturb_2022_21h <- read.csv('donnees/disturb_2022_21h.csv', header = T)
+disturb_2022_1h <- read.csv('donnees/disturb_2022_1h.csv', header = T)
+
+jj_21_1h <- read.csv('donnees/JJ_21_1h.csv', header =T)
+jj_22_1h <- read.csv('donnees/JJ_22_1h.csv', header =T)
+jj_22_21h <- read.csv('donnees/JJ_22_21h.csv', header =T)
 
 #------------------------------
 # Analyse
 #------------------------------
+library(unmarked)
+
+licat_21_1h <- subset(licat_21_1h, select = -c(Site))
+disturb_2021_1h <- subset(disturb_2021_1h, select = -c(Site))
+qualite_2021_1h <- subset(qualite_2021_1h, select = -c(Site))
+MH <- subset(MH, select = c(Enregistre, Tourbiere))
+
+#Création données unmarked
+licat_21_1h.data <- unmarkedFrameOccu(y = licat_21_1h, siteCovs = route, obsCovs = list(disturbance = disturb_2021_1h, qualite = qualite_2021_1h))
+licat_21_1h.data
+summary(licat_21_1h.data)
+
+#Modèles LICAT 2021 1h
+#Détectabilité et occupation constante
+m0 <- occu(~ 1 ~ 1, data = licat_21_1h.data)
+m0
+##Détectabilité varie, mais occupation est constante
+m1 <- occu(~ disturbance + qualite ~ 1, data = licat_21_1h.data)
+m1
+##Détectabilité varie et l'occupation varie
+m2 <- occu(~ disturbance + qualite ~ Locale, data = licat_21_1h.data)
+m2
+##Détectabilité constante et l'occupation varie
+m3 <- occu(~ 1 ~ Locale, data = licat_21_1h.data)
+m3
--- Scripts/nettoyage_donnees.R
+++ Scripts/nettoyage_donnees.R
@@ -144,11 +144,6 @@ uti_terr <- replace(uti_terr, is.na(uti_terr), 0)
 rm(codes_uti_terr)
 
 #------------------------------
-# données jours julien
-#------------------------------
-jj <-read.csv("donnees/JJ_21_21h.csv", header = T)
-
-#------------------------------
 # Création tableaux pour analyse
 #------------------------------
 
@@ -290,55 +285,245 @@ lisyl_22_21h <- subset(lisyl_22_21h, select = c(Site, V1, V2, V3, V4, V5, V6, V7
 #------------------------------
 qualite_2021_15 <- subset(anoure_2021, anoure_2021$Time24H == "1500", select = c(Site, jour_julien, RecordingQuality))
 qualite_2021_15 <- spread(qualite_2021_15, jour_julien, RecordingQuality)
-qualite_2021_15 <- replace(qualite_2021_15, is.na(qualite_2021_15), 0)
 
 qualite_2021_21 <- subset(anoure_2021, anoure_2021$Time24H == "2100", select = c(Site, jour_julien, RecordingQuality))
 qualite_2021_21 <- spread(qualite_2021_21, jour_julien, RecordingQuality)
-qualite_2021_21 <- replace(qualite_2021_21, is.na(qualite_2021_21), 0)
 
 qualite_2021_1 <- subset(anoure_2021, anoure_2021$Time24H == "100" | anoure_2021$Time24H == "0", select = c(Site, jour_julien, RecordingQuality))
 qualite_2021_1 <- spread(qualite_2021_1, jour_julien, RecordingQuality)
-qualite_2021_1 <- replace(qualite_2021_1, is.na(qualite_2021_1), 0)
+colnames(qualite_2021_1)<- c("Site","V145","V146","V148","V151","V152","V154","V155","V159","V161","V4","V5","V6","V7","V193","V194")
+for (i in 1:26) {
+  if( !is.na(qualite_2021_1$V145[i])
+  ) {qualite_2021_1$V1[i]<-qualite_2021_1$V145[i]}
+  else if( !is.na(qualite_2021_1$V146[i])
+  ) {qualite_2021_1$V1[i]<-qualite_2021_1$V146[i]}
+  else if(!is.na(qualite_2021_1$V148[i])
+  ) {qualite_2021_1$V1[i]<-qualite_2021_1$V148[i]}
+  else {qualite_2021_1$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(qualite_2021_1$V151[i])
+  ) {qualite_2021_1$V2[i]<-qualite_2021_1$V151[i]}
+  else if( !is.na(qualite_2021_1$V152[i])
+  ) {qualite_2021_1$V2[i]<-qualite_2021_1$V152[i]}
+  else if(!is.na(qualite_2021_1$V154[i])
+  ) {qualite_2021_1$V2[i]<-qualite_2021_1$V154[i]}
+  else if(!is.na(qualite_2021_1$V155[i])
+  ) {qualite_2021_1$V2[i]<-qualite_2021_1$V155[i]}
+  else {qualite_2021_1$V2[i]<-NA}
+}
+#V3
+for (i in 1:26) {
+  if( !is.na(qualite_2021_1$V159[i])
+  ) {qualite_2021_1$V3[i]<-qualite_2021_1$V159[i]}
+  else if( !is.na(qualite_2021_1$V161[i])
+  ) {qualite_2021_1$V3[i]<-qualite_2021_1$V161[i]}
+  else {qualite_2021_1$V3[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(qualite_2021_1$V193[i])
+  ) {qualite_2021_1$V8[i]<-qualite_2021_1$V193[i]}
+  else if( !is.na(qualite_2021_1$V194[i])
+  ) {qualite_2021_1$V8[i]<-qualite_2021_1$V194[i]}
+  else {qualite_2021_1$V8[i]<-NA}
+}
+
+qualite_2021_1 <- subset(qualite_2021_1, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8))
 
 ##2022##
 qualite_2022_15 <- subset(anoure_2022, anoure_2022$Time24H == "1500", select = c(Site, jour_julien, RecordingQuality))
 qualite_2022_15 <- spread(qualite_2022_15, jour_julien, RecordingQuality)
-qualite_2022_15 <- replace(qualite_2022_15, is.na(qualite_2022_15), 0)
 
 qualite_2022_21 <- subset(anoure_2022, anoure_2022$Time24H == "2100", select = c(Site, jour_julien, RecordingQuality))
 qualite_2022_21 <- spread(qualite_2022_21, jour_julien, RecordingQuality)
-qualite_2022_21 <- replace(qualite_2022_21, is.na(qualite_2022_21), 0)
+colnames(qualite_2022_21)<- c("Site","V108","V109","V111","V112","V115","V116","V3","V4","V5","V6","V7","V156","V157","V9","V10","V11","V12","V13","V14","V15","V16")
+#V1
+for (i in 1:26) {
+  if( !is.na(qualite_2022_21$V108[i])
+  ) {qualite_2022_21$V1[i]<-qualite_2022_21$V108[i]}
+  else if( !is.na(qualite_2022_21$V109[i])
+  ) {qualite_2022_21$V1[i]<-qualite_2022_21$V109[i]}
+  else if(!is.na(qualite_2022_21$V111[i])
+  ) {qualite_2022_21$V1[i]<-qualite_2022_21$V111[i]}
+  else if(!is.na(qualite_2022_21$V112[i])
+  ) {qualite_2022_21$V1[i]<-qualite_2022_21$V112[i]}
+  else {qualite_2022_21$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(qualite_2022_21$V115[i])
+  ) {qualite_2022_21$V2[i]<-qualite_2022_21$V115[i]}
+  else if( !is.na(qualite_2022_21$V116[i])
+  ) {qualite_2022_21$V2[i]<-qualite_2022_21$V116[i]}
+  else {qualite_2022_21$V2[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(qualite_2022_21$V156[i])
+  ) {qualite_2022_21$V8[i]<-qualite_2022_21$V156[i]}
+  else if( !is.na(qualite_2022_21$V157[i])
+  ) {qualite_2022_21$V8[i]<-qualite_2022_21$V157[i]}
+  else {qualite_2022_21$V8[i]<-NA}
+}
+qualite_2022_21 <- subset(qualite_2022_21, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16))
 
 qualite_2022_1 <- subset(anoure_2022, anoure_2022$Time24H == "100" | anoure_2022$Time24H == "0", select = c(Site, jour_julien, RecordingQuality))
 qualite_2022_1 <- spread(qualite_2022_1, jour_julien, RecordingQuality)
-qualite_2022_1 <- replace(qualite_2022_1, is.na(qualite_2022_1), 0)
+colnames(qualite_2022_1)<- c("Site","V109","V110","V112","V113","V116","V117","V3","V4","V5","V6","V7","V157","V158","V9","V10","V11","V12","V13","V14","V15","V16")
+#V1
+for (i in 1:26) {
+  if( !is.na(qualite_2022_1$V109[i])
+  ) {qualite_2022_1$V1[i]<-qualite_2022_1$V109[i]}
+  else if( !is.na(qualite_2022_1$V110[i])
+  ) {qualite_2022_1$V1[i]<-qualite_2022_1$V110[i]}
+  else if(!is.na(qualite_2022_1$V112[i])
+  ) {qualite_2022_1$V1[i]<-qualite_2022_1$V112[i]}
+  else if(!is.na(qualite_2022_1$V113[i])
+  ) {qualite_2022_1$V1[i]<-qualite_2022_1$V113[i]}
+  else {qualite_2022_1$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(qualite_2022_1$V116[i])
+  ) {qualite_2022_1$V2[i]<-qualite_2022_1$V116[i]}
+  else if( !is.na(qualite_2022_1$V117[i])
+  ) {qualite_2022_1$V2[i]<-qualite_2022_1$V117[i]}
+  else {qualite_2022_1$V2[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(qualite_2022_1$V157[i])
+  ) {qualite_2022_1$V8[i]<-qualite_2022_1$V157[i]}
+  else if( !is.na(qualite_2022_1$V158[i])
+  ) {qualite_2022_1$V8[i]<-qualite_2022_1$V158[i]}
+  else {qualite_2022_1$V8[i]<-NA}
+}
+qualite_2022_1 <- subset(qualite_2022_1, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16))
+
 
 #Type de disturbance
 disturb_2021_15 <- subset(anoure_2021, anoure_2021$Time24H == "1500", select = c(Site, jour_julien, DisturbanceType))
 disturb_2021_15 <- spread(disturb_2021_15, jour_julien, DisturbanceType)
-disturb_2021_15 <- replace(disturb_2021_15, is.na(disturb_2021_15), 0)
 
 disturb_2021_21 <- subset(anoure_2021, anoure_2021$Time24H == "2100", select = c(Site, jour_julien, DisturbanceType))
 disturb_2021_21 <- spread(disturb_2021_21, jour_julien, DisturbanceType)
-disturb_2021_21 <- replace(disturb_2021_21, is.na(disturb_2021_21), 0)
 
 disturb_2021_1 <- subset(anoure_2021, anoure_2021$Time24H == "100" | anoure_2021$Time24H == "0", select = c(Site, jour_julien, DisturbanceType))
 disturb_2021_1 <- spread(disturb_2021_1, jour_julien, DisturbanceType)
-disturb_2021_1 <- replace(disturb_2021_1, is.na(disturb_2021_1), 0)
+colnames(disturb_2021_1)<- c("Site","V145","V146","V148","V151","V152","V154","V155","V159","V161","V4","V5","V6","V7","V193","V194")
+for (i in 1:26) {
+  if( !is.na(disturb_2021_1$V145[i])
+  ) {disturb_2021_1$V1[i]<-disturb_2021_1$V145[i]}
+  else if( !is.na(disturb_2021_1$V146[i])
+  ) {disturb_2021_1$V1[i]<-disturb_2021_1$V146[i]}
+  else if(!is.na(disturb_2021_1$V148[i])
+  ) {disturb_2021_1$V1[i]<-disturb_2021_1$V148[i]}
+  else {disturb_2021_1$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(disturb_2021_1$V151[i])
+  ) {disturb_2021_1$V2[i]<-disturb_2021_1$V151[i]}
+  else if( !is.na(disturb_2021_1$V152[i])
+  ) {disturb_2021_1$V2[i]<-disturb_2021_1$V152[i]}
+  else if(!is.na(disturb_2021_1$V154[i])
+  ) {disturb_2021_1$V2[i]<-disturb_2021_1$V154[i]}
+  else if(!is.na(disturb_2021_1$V155[i])
+  ) {disturb_2021_1$V2[i]<-disturb_2021_1$V155[i]}
+  else {disturb_2021_1$V2[i]<-NA}
+}
+#V3
+for (i in 1:26) {
+  if( !is.na(disturb_2021_1$V159[i])
+  ) {disturb_2021_1$V3[i]<-disturb_2021_1$V159[i]}
+  else if( !is.na(disturb_2021_1$V161[i])
+  ) {disturb_2021_1$V3[i]<-disturb_2021_1$V161[i]}
+  else {disturb_2021_1$V3[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(disturb_2021_1$V193[i])
+  ) {disturb_2021_1$V8[i]<-disturb_2021_1$V193[i]}
+  else if( !is.na(disturb_2021_1$V194[i])
+  ) {disturb_2021_1$V8[i]<-disturb_2021_1$V194[i]}
+  else {disturb_2021_1$V8[i]<-NA}
+}
+disturb_2021_1 <- subset(disturb_2021_1, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8))
+
 
 #2022
 
 disturb_2022_15 <- subset(anoure_2022, anoure_2022$Time24H == "1500", select = c(Site, jour_julien, DisturbanceType))
 disturb_2022_15 <- spread(disturb_2022_15, jour_julien, DisturbanceType)
-disturb_2022_15 <- replace(disturb_2022_15, is.na(disturb_2022_15), 0)
 
 disturb_2022_21 <- subset(anoure_2022, anoure_2022$Time24H == "2100", select = c(Site, jour_julien, DisturbanceType))
 disturb_2022_21 <- spread(disturb_2022_21, jour_julien, DisturbanceType)
-disturb_2022_21 <- replace(disturb_2022_21, is.na(disturb_2022_21), 0)
+colnames(disturb_2022_21)<- c("Site","V108","V109","V111","V112","V115","V116","V3","V4","V5","V6","V7","V156","V157","V9","V10","V11","V12","V13","V14","V15","V16")
+#V1
+for (i in 1:26) {
+  if( !is.na(disturb_2022_21$V108[i])
+  ) {disturb_2022_21$V1[i]<-disturb_2022_21$V108[i]}
+  else if( !is.na(disturb_2022_21$V109[i])
+  ) {disturb_2022_21$V1[i]<-disturb_2022_21$V109[i]}
+  else if(!is.na(disturb_2022_21$V111[i])
+  ) {disturb_2022_21$V1[i]<-disturb_2022_21$V111[i]}
+  else if(!is.na(disturb_2022_21$V112[i])
+  ) {disturb_2022_21$V1[i]<-disturb_2022_21$V112[i]}
+  else {disturb_2022_21$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(disturb_2022_21$V115[i])
+  ) {disturb_2022_21$V2[i]<-disturb_2022_21$V115[i]}
+  else if( !is.na(disturb_2022_21$V116[i])
+  ) {disturb_2022_21$V2[i]<-disturb_2022_21$V116[i]}
+  else {disturb_2022_21$V2[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(disturb_2022_21$V156[i])
+  ) {disturb_2022_21$V8[i]<-disturb_2022_21$V156[i]}
+  else if( !is.na(disturb_2022_21$V157[i])
+  ) {disturb_2022_21$V8[i]<-disturb_2022_21$V157[i]}
+  else {disturb_2022_21$V8[i]<-NA}
+}
+disturb_2022_21 <- subset(disturb_2022_21, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16))
+
 
 disturb_2022_1 <- subset(anoure_2022, anoure_2022$Time24H == "100" | anoure_2022$Time24H == "0", select = c(Site, jour_julien, DisturbanceType))
 disturb_2022_1 <- spread(disturb_2022_1, jour_julien, DisturbanceType)
-disturb_2022_1 <- replace(disturb_2022_1, is.na(disturb_2022_1), 0)
+colnames(disturb_2022_1)<- c("Site","V109","V110","V112","V113","V116","V117","V3","V4","V5","V6","V7","V157","V158","V9","V10","V11","V12","V13","V14","V15","V16")
+#V1
+for (i in 1:26) {
+  if( !is.na(disturb_2022_1$V109[i])
+  ) {disturb_2022_1$V1[i]<-disturb_2022_1$V109[i]}
+  else if( !is.na(disturb_2022_1$V110[i])
+  ) {disturb_2022_1$V1[i]<-disturb_2022_1$V110[i]}
+  else if(!is.na(disturb_2022_1$V112[i])
+  ) {disturb_2022_1$V1[i]<-disturb_2022_1$V112[i]}
+  else if(!is.na(disturb_2022_1$V113[i])
+  ) {disturb_2022_1$V1[i]<-disturb_2022_1$V113[i]}
+  else {disturb_2022_1$V1[i]<-NA}
+}
+#V2
+for (i in 1:26) {
+  if( !is.na(disturb_2022_1$V116[i])
+  ) {disturb_2022_1$V2[i]<-disturb_2022_1$V116[i]}
+  else if( !is.na(disturb_2022_1$V117[i])
+  ) {disturb_2022_1$V2[i]<-disturb_2022_1$V117[i]}
+  else {disturb_2022_1$V2[i]<-NA}
+}
+#V8
+for (i in 1:26) {
+  if( !is.na(disturb_2022_1$V157[i])
+  ) {disturb_2022_1$V8[i]<-disturb_2022_1$V157[i]}
+  else if( !is.na(disturb_2022_1$V158[i])
+  ) {disturb_2022_1$V8[i]<-disturb_2022_1$V158[i]}
+  else {disturb_2022_1$V8[i]<-NA}
+}
+disturb_2022_1 <- subset(disturb_2022_1, select = c(Site, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16))
 
 #------------------------------
 # Enregistrement données nettoyées
